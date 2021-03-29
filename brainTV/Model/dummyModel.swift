0afaec5e40f39b44e518ce75dd8aae527742f504
João Brentano <joaobrentano.br@gmail.com>
2021-03-29 15:08:20 -0300

    dummy files

---
+++ brainTV/Model/dummyModel.swift
@@ -0,0 +1,20 @@
+//
+//  dummyModel.swift
+//  brainTV
+//
+//  Created by João Brentano on 29/03/21.
+//
+
+import SwiftUI
+
+struct dummyModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct dummyModel_Previews: PreviewProvider {
+    static var previews: some View {
+        dummyModel()
+    }
+}
