9e7e18c6d11a1f9327d8bdbe621222d0e2c2fc35
João Brentano <joaobrentano.br@gmail.com>
2021-03-29 14:45:38 -0300

    Initial Commit

---
+++ brainTV/brainTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  brainTVApp.swift
+//  brainTV
+//
+//  Created by João Brentano on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct brainTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
