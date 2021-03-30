99c01b3ffa553cd03873378c3fc0f2bc64d38a56
FelipeOliveira <Felipe.Oliveira01@edu.pucrs.br>
2021-03-30 15:36:46 -0300

    FilterView

---
+++ brainTV/Views/FilterNavBarView.swift
@@ -0,0 +1,51 @@
+//
+//  FilterNavBarView.swift
+//  brainTV
+//
+//  Created by Felipe Grosze Nipper de Oliveira on 30/03/21.
+//
+
+import SwiftUI
+
+struct Tag: Identifiable {
+    var id: String { name }
+    var name: String
+    var index:Int
+}
+
+struct FilterNavBarView: View {
+    var tagsEntity:[Tag] = []
+    init(tags:[String]) {
+        for k in 0...tags.count-1 {
+            tagsEntity.append(Tag(name: tags[k],index:k))
+        }
+    }
+    @State private var isPressed = 0
+    var body: some View {
+        ScrollView(.horizontal,showsIndicators:
+                    false) {
+            HStack{
+                Spacer()
+                ForEach(tagsEntity){ tag in
+                    Button(tag.name){
+                        isPressed = tag.index
+                    }
+                    .padding()
+                    .frame(width: /*@START_MENU_TOKEN@*/113.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/60.0/*@END_MENU_TOKEN@*/)
+                    .background(isPressed == tag.index ? Color.red : Color(.sRGB, red: 217/255, green: 44/255, blue: 45/255, opacity: 0))
+                        .foregroundColor(isPressed == tag.index ? Color.white : Color.black)
+                        .font(.title)
+                    .cornerRadius(40)
+                }
+            }
+        }
+        
+        
+    }
+}
+
+struct FilterNavBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterNavBarView(tags: ["teste1","teste2","teste3","teste4"])
+    }
+}
