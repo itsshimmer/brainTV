11996c8d2c73c37124a3180a1918fd3eac666ac9
FelipeOliveira <Felipe.Oliveira01@edu.pucrs.br>
2021-03-30 17:31:05 -0300

    v1

--- brainTV/Views/PanelistCardView.swift
+++ brainTV/Views/PanelistCardView.swift
@@ -11,21 +11,23 @@ struct PanelistCardView: View {
     var doctor: Doctor
     
     var body: some View {
-        HStack {
-            doctor.image
-                .resizable()
-                .frame(width: 98, height: 107, alignment: .leading)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-            VStack(alignment: .leading) {
-                Text(doctor.name)
-                    .bold()
-                    .font(.title2)
-                Text(doctor.speciality)
-                StandardButtonView(width: 100, height: 20, backgroundColor: .red, text: "Ver mais", textColor: .white)
+            HStack {
+                doctor.image
+                    .resizable()
+                    .frame(width: 98, height: 107, alignment: .leading)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                VStack(alignment: .leading) {
+                    Text(doctor.name)
+                        .foregroundColor(Color.black)
+                        .bold()
+                        .font(.title2)
+                    Text(doctor.speciality)
+                        .foregroundColor(Color.black)
+                        StandardButtonView(width: 100, height: 20, backgroundColor: .red, text: "Ver mais", textColor: .white)
+                }
             }
+            .frame(width: UIScreen.main.bounds.width*0.9, height: 107, alignment: .leading)
         }
-        .frame(width: UIScreen.main.bounds.width*0.9, height: 107, alignment: .leading)
-    }
 }
 
 struct PanelistCardView_Previews: PreviewProvider {
