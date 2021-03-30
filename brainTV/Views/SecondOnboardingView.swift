39d0ef2bdd81c4026b5121136cde01731b77e6ca
lccxm <>
2021-03-30 16:14:35 -0300

    secondAndThirdOnboardingViews

---
+++ brainTV/Views/SecondOnboardingView.swift
@@ -0,0 +1,40 @@
+//
+//  SecondOnboardingView.swift
+//  brainTV
+//
+//  Created by Lucca Molon on 30/03/21.
+//
+
+import SwiftUI
+
+struct SecondOnboardingView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .ignoresSafeArea()
+            VStack {
+                logoView()
+                    .frame(width: 310, height: 150, alignment: .center)
+                Spacer()
+                Image("Hand_Tablet_1")
+                    .resizable()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .padding(.top, -70.0)
+                Spacer()
+                Text("As aulas ficam gravadas e são disponibilizadas aqui no App também.")
+                    .font(.title2)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, -100.0)
+                    .padding(.horizontal, 76.0)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct SecondOnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondOnboardingView()
+    }
+}
---
+++ brainTV/Views/ThirdOnboardingView.swift
@@ -0,0 +1,42 @@
+//
+//  ThirdOnboardingView.swift
+//  brainTV
+//
+//  Created by Lucca Molon on 30/03/21.
+//
+
+import SwiftUI
+
+struct ThirdOnboardingView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .ignoresSafeArea()
+            VStack {
+                logoView()
+                    .frame(width: 310, height: 150, alignment: .center)
+                Spacer()
+                Image("Handshake_1")
+                    .resizable()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .padding(.top, -70.0)
+                Spacer()
+                Text("Agora é com você! Aproveite esta experiência digital.")
+                    .font(.title2)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, -100.0)
+                    .padding(.horizontal, 76.0)
+                    .padding(.bottom, -40.0)
+                StandardButtonView(width: 156, height:30, backgroundColor: .red, text: "Começar", textColor: .white, image: "")
+                    .padding(.bottom, 120.0)
+            }
+        }
+    }
+}
+
+struct ThirdOnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdOnboardingView()
+    }
+}
