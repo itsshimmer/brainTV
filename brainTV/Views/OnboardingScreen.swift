74bfd183f578d0aac7bfbf7ac521f3926805d1e3
João Brentano <joaobrentano.br@gmail.com>
2021-03-31 16:03:59 -0300

    Navigation update

--- brainTV/Views/OnboardingScreen.swift
+++ brainTV/Views/OnboardingScreen.swift
@@ -8,25 +8,32 @@
 import SwiftUI
 
 struct OnboardingScreen: View {
+    
+    @State var isActive:Bool = false
+    
     var body: some View {
         let yExtension: CGFloat = 55
         ZStack{
-            GeometryReader { g in
-                TabView {
-                    FirstOnboardingView()
-                    SecondOnboardingView()
-                    ThirdOnboardingView()
+            if self.isActive {
+                LoginScreen()
+            } else {
+                GeometryReader { g in
+                    TabView {
+                        FirstOnboardingView()
+                        SecondOnboardingView()
+                        ThirdOnboardingView(end: $isActive)
+                    }
+                    .frame(width: g.size.width, height: g.size.height + yExtension)
+                    .tabViewStyle(PageTabViewStyle())
                 }
-                .frame(width: g.size.width, height: g.size.height + yExtension)
-                .tabViewStyle(PageTabViewStyle())
-            }
-            .offset(y: -yExtension)
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            VStack{
-                logoView()
-                    .frame(width: 310, height: 150, alignment: .center)
-                    .padding(.top, 50.0)
-                Spacer()
+                .offset(y: -yExtension)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack{
+                    logoView()
+                        .frame(width: 310, height: 150, alignment: .center)
+                        .padding(.top, 50.0)
+                    Spacer()
+                }
             }
         }
     }
